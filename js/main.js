3e84fb456b7796c56c03187570fe14f869fac605
hexeva <patrick79.g@gmail.com>
2021-11-15 17:26:09 +0100

    added close menu event

--- js/main.js
+++ js/main.js
@@ -1,14 +1,27 @@
 const myHamburger = document.querySelector('.header-right > a');
-// console.log(myHamburger);
 
 
 
-myHamburger.addEventListener ('click', function () {
+// creo evento al click
+myHamburger.addEventListener ('click', 
+function () {
 
     let clickHamburger = document.querySelector('.hamburger-menu');
 
-
-    clickHamburger.classList.add('.active');
+    clickHamburger.classList.add('active');
     console.log(clickHamburger);
+});
+
+
+const closeX = document.querySelector('.close');
+
+// creo evento al click
+
+closeX.addEventListener ('click',
+function () {
+
+    let closeMenu = document.querySelector('.hamburger-menu-active');
+    closeMenu.classList.remove('active');
+    console.log(closeMenu);
 });
 
