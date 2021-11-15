5840213eeeca653244ba172da8c8b37400411ac1
hexeva <patrick79.g@gmail.com>
2021-11-15 16:39:27 +0100

    add event listener

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ const myHamburger = document.querySelector('.header-right > a');
 console.log(myHamburger);
 
 myHamburger.addEventListener ('click', function () {
-document.querySelector('.hamburger-menu');
+document.querySelector('.hamburger-menu').classList.add('.active');
+console.log(hamburger-menu);
 
 });
 
