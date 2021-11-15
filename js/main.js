9989034f23b589ea6b328ed0173617b601713ffa
hexeva <patrick79.g@gmail.com>
2021-11-15 16:57:49 +0100

    add active

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const myHamburger = document.querySelector('.header-right > a');
-console.log(myHamburger);
+// console.log(myHamburger);
+
+
 
 myHamburger.addEventListener ('click', function () {
-document.querySelector('.hamburger-menu').classList.add('.active');
-console.log(hamburger-menu);
 
+    let clickHamburger = document.querySelector('.hamburger-menu');
+
+
+    clickHamburger.classList.add('.active');
+    console.log(clickHamburger);
 });
 
